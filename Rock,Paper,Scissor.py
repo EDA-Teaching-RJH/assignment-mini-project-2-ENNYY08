5ef7030712b127d85b4285f781ee181f524bd566
ENNYY08 <med10@kent.ac.uk>
2026-03-05 18:47:03 +0000

    rounds

--- Rock,Paper,Scissor.py
+++ Rock,Paper,Scissor.py
@@ -15,10 +15,22 @@ def display_meun():
     print("5. Remove Player Records")
     print("6. Exit")
 
+    return input("Select an option: ")
     
-# Welcome Player to the game, have them introduce themesleve, and pick who they will like to play aganist
+# Welcome Player to the game, have them introduce themesleve, and pick who they will like to play aganist and how many rounds 
 
 def play_game(): 
     name = input("Enter your name")
     print("Welcome", name + "!")
     print("You are Player 1.")
+
+def get_round(): 
+    while True: 
+        try: 
+            rounds = int(input("How many rounds? (1-10):"))
+            if 1<= round >=10:
+                return rounds
+            else:
+                print("Enter a number between 1 and 10.")
+        except:
+            print("You have entered an Invalid input. Enter a Number")
